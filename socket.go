a61b06438e170b720420a11d43687a65e4fd976c
Rakibul Yeasin <ryeasin03@gmail.com>
2022-08-02 00:44:14 +0600

    Return complete url on network type

---
+++ socket.go
@@ -0,0 +1,21 @@
+package seadisc
+
+import "strings"
+
+var (
+	BaseUrl string = "wss://{network}stream.openseabeta.com/socket/websocket?token="
+)
+
+func GetNetwork(network string) string {
+	var (
+		net string = ""
+		url string
+	)
+
+	if strings.ToLower(network) != "main" {
+		net = "testnets-"
+	}
+
+	url = strings.Replace(BaseUrl, "{network}", net, 1)
+	return url
+}
